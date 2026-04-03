6c22ba46f7a08f20e6093c879a9492fcaf676dc8
Teddy Li <teddyxlandlee@hotmail.com>
2026-04-03 15:11:33 +0800

    Schema v2: current_hooks (Level 1) and all_hooks (Level 2)

--- src/main/java/xland/mcmod/enchlevellangpatch/impl/telemetry/TelemetryData.java
+++ src/main/java/xland/mcmod/enchlevellangpatch/impl/telemetry/TelemetryData.java
@@ -1,6 +1,11 @@
 package xland.mcmod.enchlevellangpatch.impl.telemetry;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import xland.mcmod.enchlevellangpatch.api.EnchantmentLevelLangPatchConfig;
+import xland.mcmod.enchlevellangpatch.impl.LangPatchImpl;
+
+import java.util.Map;
 
 public class TelemetryData {
     static final int TELEMETRY_SCHEMA = 1;
@@ -19,13 +24,36 @@ public class TelemetryData {
         obj.addProperty("mod_version", Platform.CURRENT.getModVersion());
         obj.addProperty("mod_platform", Platform.CURRENT.getName());
         obj.addProperty("mc_version", Platform.CURRENT.getMinecraftVersion());
+
+        obj.add("current_hooks", getCurrentHooksInfo());
+        return obj;
+    }
+
+    private static JsonObject getCurrentHooksInfo() {
+        JsonObject obj = new JsonObject();
+        obj.addProperty("enchantment", LangPatchImpl.ENCHANTMENT_HOOK.getId(EnchantmentLevelLangPatchConfig.getCurrentEnchantmentHooks()).toString());
+        obj.addProperty("potion", LangPatchImpl.POTION_HOOK.getId(EnchantmentLevelLangPatchConfig.getCurrentPotionHooks()).toString());
         return obj;
     }
 
     public static JsonObject getFull() {
         JsonObject obj =  getFunctional();
         obj.addProperty("telemetry_level", LEVEL_OPTIONAL);
+        obj.add("all_hooks", getAllHooksInfo());
         return obj;
+    }
+
+    private static JsonObject getAllHooksInfo() {
+        JsonObject obj = new JsonObject();
+        obj.add("enchantment", keySetToJsonArray(EnchantmentLevelLangPatchConfig.getEnchantmentHooksContext()));
+        obj.add("potion", keySetToJsonArray(EnchantmentLevelLangPatchConfig.getPotionHooksContext()));
+        return obj;
+    }
+
+    private static JsonArray keySetToJsonArray(Map<? extends String, ?> map) {
+        JsonArray arr = new JsonArray();
+        map.keySet().forEach(arr::add);
+        return arr;
     }
 
     static final int LEVEL_MANDATORY = 0;
--- src/main/java/xland/mcmod/enchlevellangpatch/impl/telemetry/package-info.java
+++ src/main/java/xland/mcmod/enchlevellangpatch/impl/telemetry/package-info.java
@@ -1,3 +1,10 @@
+/**
+ * Telemetry code. You can configure telemetry level or disable it by creating
+ * {@code enchlevel-langpatch-telemetry.txt} config file.
+ *
+ * @see <a href="https://github.com/teddyxlandlee/langpatch-telemetry/blob/master/SCHEMA.txt">
+ *     Telemetry schema</a>
+ */
 @API(status = API.Status.INTERNAL)
 package xland.mcmod.enchlevellangpatch.impl.telemetry;
 
